12f4b0675c2d3c6cba69418641a4487b61e0d71f
ramonaN <ramona.nechita99@gmail.com>
2021-01-15 17:57:27 +0200

    react-confusion

---
+++ React/confusion/src/components/DishdetailComponent.js
@@ -0,0 +1,204 @@
+import {React,Component} from 'react';
+import { Breadcrumb, BreadcrumbItem,Card, CardImg,CardText, CardBody, CardTitle,Button ,Modal, ModalHeader, ModalBody, Row,Col,Label} from 'reactstrap';
+import {Link} from 'react-router-dom';
+import {Control, LocalForm,Errors} from 'react-redux-form';
+import {addComment, postComment} from '../redux/ActionCreators';
+import {Loading} from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl';
+import {FadeTransform,Fade, Stagger} from 'react-animation-components';
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+  function RenderDish({dish}){
+        if(dish!=null){
+            return(
+                <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                <Card>
+                <CardImg width="100%" object="true" src={baseUrl+dish.image} alt={dish.name} />
+                <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+                </CardBody>
+                </Card>
+                </FadeTransform>
+            )
+        }
+        else{
+            return (<div></div>)
+        }
+    }
+    
+  function  RenderComments({comments, postComment, dishId}){
+   
+     if (comments!=null){
+         const comms=comments.map(comm=>
+            <Fade in>
+               <li className="mb-3">{comm.comment}<br/>
+                --{comm.author},{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comm.date)))} 
+            <br></br></li> </Fade>)
+      
+         return(
+             <div className="container">
+                 <h2>Comments</h2>
+                  <ul className='list-unstyled'><Stagger in>{comms}</Stagger></ul>
+                 <CommentForm  dishId={dishId} postComment={postComment} addComment={addComment}/>
+
+             </div>
+         
+            )
+     }
+     else{
+         return(<div></div>)
+     }
+    }
+
+   
+      
+        
+            
+        
+    
+    class CommentForm extends Component{
+        constructor(props){
+            super(props);
+            this.state={
+                isModalOpen:false
+            }
+            this.toggleModal=this.toggleModal.bind(this);
+            this.handleSubmit=this.handleSubmit.bind(this);
+        }
+        toggleModal(){
+            this.setState({
+                isModalOpen: !this.state.isModalOpen
+            })
+        }
+        handleSubmit(values) {
+
+            this.toggleModal();
+            this.props.addComment(this.props.dishId, values.rating, values.author, values.comment)
+            this.props.postComment(this.props.dishId, values.rating, values.author, values.comment)
+
+            // event.preventDefault();
+        }
+        render(){
+            return(
+                <div>
+                <Button outline onClick={this.toggleModal}><span className="fa fa-pencil fa-lg"></span> Submit Comment</Button>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                <ModalBody>
+                    <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                        <Row className="form-group">
+                            <Col>
+                            <Label htmlFor="rating">Rating</Label>
+                            <Control.select model=".rating" id="rating" className="form-control">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </Control.select>
+                            </Col>
+                        </Row>
+                        <Row className="form-group">
+                                <Label htmlFor="author" md={12}>Your Name</Label>
+                                <Col md={12}>
+                                    <Control.text model=".author" id="author" name="author"
+                                        placeholder="Your name"
+                                        className="form-control"
+                                        validators={{
+                                         minLength: minLength(3), maxLength: maxLength(15)
+                                        }}
+                                         />
+                                         <Errors
+                                        className="text-danger"
+                                        model="author"
+                                        show="touched"
+                                        messages={{
+                                            minLength: 'Must be greater than 3 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                     />
+                                   
+                                </Col>
+                            </Row>
+                        <Row className="form-group">
+                            <Col>
+                            <Label htmlFor="comment">Comment</Label>
+                            <Control.textarea model=".comment" id="comment"
+                                        rows="6" className="form-control" />
+                            </Col>
+                        </Row>
+                        <Button type="submit" className="bg-primary">
+                            Submit
+                        </Button>
+                    </LocalForm>
+                </ModalBody>
+             
+      </Modal>
+      </div>
+                           
+            )
+        }
+        
+    }
+    const DishDetail=(props)=>{
+  
+        if(props.isLoading)
+        {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <Loading/>
+                    </div>
+                </div>
+            )
+        }
+        else if(props.errMess){
+            return(
+                <div className="container">
+                    <div className="row">
+    <h4>{this.props.errMess}</h4></div>                </div>
+            )
+        }
+       else  if(props.dish!=null)
+                return(
+                    <div className="container">
+                    <div className="row">
+                        <Breadcrumb>
+    
+                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>                
+                    </div>
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderDish dish={props.dish} />
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderComments comments={props.comments} 
+                            postComment={props.postComment}
+                            addComment={props.addComment}
+                            dishId={props.dish.id} />
+                           
+                            
+                  
+
+                        </div>
+                    </div>
+                    
+                  
+                    </div>
+                   
+                )
+       
+    }
+
+export default DishDetail;
---
+++ React/confusion/src/redux/comments.js
@@ -0,0 +1,14 @@
+import * as ActionTypes from './ActionTypes';
+export const Comments= (state={errMess:null,
+comments:[]
+}, action)=>{
+    switch(action.type){
+        case ActionTypes.ADD_COMMENTS:
+            return {...state,errMess:null, comments:action.payload}
+        case ActionTypes.ADD_COMMENT:
+            var comment=action.payload;
+            return {...state,comments:state.comments.concat(comment)};
+        default:
+            return state;
+    }
+}
